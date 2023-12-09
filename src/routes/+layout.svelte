d77d1541bc2b452651f9daac46e99d2dcc5a414e
kelvinwasike007 <kelvinwasike007@gmail.com>
2023-12-08 22:34:29 -0800

    start

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,65 @@
+<script>
+    import "../app.css"
+//   import { browser } from '$app/environment';
+//     import * as handTrack from 'handtrackjs';
+//   import { onMount } from 'svelte';
+
+//     let canvas;
+//     let ctx
+//   let videoSource = null;
+//   let loading = false;
+//   const defaultParams = {
+//     flipHorizontal: false,
+//     outputStride: 16,
+//     imageScaleFactor: 1,
+//     maxNumBoxes: 20,
+//     iouThreshold: 0.2,
+//     scoreThreshold: 0.6,
+//     modelType: "ssd640fpnlite",
+//     modelSize: "large",
+//     bboxLineWidth: "2",
+//     fontSize: 17,
+// };
+
+// const model = async ()=>await handTrack.load(defaultParams)
+// const mod = model()
+//   const obtenerVideoCamara = async () => {
+//     try {
+//       loading = true;
+//       const stream = await navigator.mediaDevices.getUserMedia({
+//         video: true,
+//       });
+//       videoSource.srcObject = stream;
+//       videoSource.play()
+//       loading = false;
+      
+      
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   function handleCanvas(src){
+//     setInterval(() => {
+//         captureFrame()
+//     }, 1);
+//   }
+//    const captureFrame = async () => {
+//         if (videoSource && canvas) {
+//             ctx.drawImage(videoSource, 0, 0, canvas.width, canvas.height);
+//             const imageData = canvas.toDataURL('image/png');
+//             console.log(await (await mod).detect(canvas))
+//             // console.log('Captured frame as image:', imageData);
+//         }
+//     };
+//   if(browser){
+//     obtenerVideoCamara()
+
+
+//   }
+//  onMount(() => {
+// 		ctx = canvas.getContext('2d')
+// 		ctx.width = 400
+//         ctx.height = 400
+// 	})
+</script>
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="h-screen w-screen ">
+    <div class="flex flex-col justify-center items-center w-full h-full bg-blue-500">
+        <div>VIRTUAL ART EXHIBITION</div>
+        <div>CURRENT PORTAIT</div>
+        <div>LIST PORTAIT</div>
+    </div>
+</div>
